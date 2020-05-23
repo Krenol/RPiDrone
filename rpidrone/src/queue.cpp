b5ab56c86a001c9815f015d70078127587bf8036
Krenol <lauritzgroeger@gmail.com>
2020-05-23 15:29:36 +0200

    updated queue & added code comments

--- rpidrone/src/queue.cpp
+++ rpidrone/src/queue.cpp
@@ -3,6 +3,14 @@
 namespace rpidrone
 {
     template<class T>
+    void Queue<T>::intern_pop(T& item) 
+    {
+        item = queue_.front();
+        last_value_ = item;
+        queue_.pop();
+    }
+
+    template<class T>
     void Queue<T>::push(const T& item) 
     {
         std::unique_lock<std::mutex> mlock(mutex_);
@@ -15,25 +23,51 @@ namespace rpidrone
     T Queue<T>::pop() 
     {
         std::unique_lock<std::mutex> mlock(mutex_);
+        T val;
         while (queue_.empty())
         {
             cond_.wait(mlock);
         }
-        auto val = queue_.front();
-        queue_.pop();
+        intern_pop(val);
         return val;
     }
+    
+    template<class T>
+    T Queue<T>::pop_or_last() 
+    {
+        std::unique_lock<std::mutex> mlock(mutex_);
+        T val;
+        if(queue_.empty()){
+            val = last_value_;
+        } else {
+            intern_pop(val);
+        }
+        return val;
+    }
+
+    template<class T>
+    void Queue<T>::pop_or_last(T& item) 
+    {
+        std::unique_lock<std::mutex> mlock(mutex_);
+        if(queue_.empty()){
+            item = last_value_;
+        } else {
+            intern_pop(item);
+        }
+    }
+    
     
     template<class T>
     void Queue<T>::pop(T& item) 
     {
         std::unique_lock<std::mutex> mlock(mutex_);
+        
         while (queue_.empty())
         {
             cond_.wait(mlock);
         }
-        item = queue_.front();
-        queue_.pop();
+
+        intern_pop(item);
     }
     
     template<class T>
