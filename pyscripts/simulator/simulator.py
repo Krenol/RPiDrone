a32c5eda29dd4068049b84dd31bd53ed27c627f6
krenol <lauritzgroeger@gmail.com>
2021-09-04 16:40:31 +0200

    add is and should angles to output

--- pyscripts/simulator/simulator.py
+++ pyscripts/simulator/simulator.py
@@ -7,6 +7,10 @@ import thread
 HOSTNAME = "localhost"
 PORT = 8889
 CONTEXT = "/"
+UPDATE_JSON = {"controls":{"escs":{"controllers":{"k_i":{"roll_rate":0,"pitch_rate":0,"roll_output":0,"pitch_output":0,"yaw_output":0},"k_p":{"roll_rate":1,"pitch_rate":1,"roll_output":1,"pitch_output":1,"yaw_output":1},"k_d":{"roll_rate":0,"pitch_rate":0,"roll_output":0,"pitch_output":0,"yaw_output":0},"k_aw":{"roll_rate":0,"pitch_rate":0,"roll_output":0,"pitch_output":0,"yaw_output":0},"max_diff":{"roll_rate":8,"pitch_rate":8,"roll_output":50,"pitch_output":50,"yaw_output":25}}}}}
+BEST_JSON = {"controls":{"escs":{"controllers":{"k_i":{"roll_rate":0,"pitch_rate":0,"roll_output":0,"pitch_output":0,"yaw_output":0},"k_p":{"roll_rate":1,"pitch_rate":1,"roll_output":1,"pitch_output":1,"yaw_output":1},"k_d":{"roll_rate":0,"pitch_rate":0,"roll_output":0,"pitch_output":0,"yaw_output":0},"k_aw":{"roll_rate":0,"pitch_rate":0,"roll_output":0,"pitch_output":0,"yaw_output":0},"max_diff":{"roll_rate":8,"pitch_rate":8,"roll_output":50,"pitch_output":50,"yaw_output":25}}}}}
+DATA = []
+
 
 def send(ws, deg: float = 0, offset: float = 0, rotation: float = 0, throttle: int = 0):
     data = {"gps":{"altitude":0,"latitude":0,"longitude":0},"joystick":{"degrees": deg,"offset": offset, "rotation": rotation},"throttle": throttle}
@@ -22,6 +26,7 @@ def read(t: tuple):
             time.sleep(1)
         j = json.loads(data)
         print(j)
+        DATA.append(j)
     except Exception:
         time.sleep(1)
 
@@ -31,6 +36,7 @@ def run():
         while True:
             try:
                 ws = create_connection("ws://" + HOSTNAME + ":" + PORT + CONTEXT)
+                DATA.clear()
                 break
             except KeyboardInterrupt:
                 return
